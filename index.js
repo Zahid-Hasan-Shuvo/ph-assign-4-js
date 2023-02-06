36cef6a8f6d55126668fe5e12bb3a0093c2c6498
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-02-06 22:38:36 +0600

    first problem done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Problem 1
+// Problem 1. Here gives an function that takes an positive number and doing some arithmetical operation. first mul with that num then add plus 10 ,then divided by 2. and at last sub by 5
+
+
+function mindGame(num){
+  let mulResult=((num*3)+10)/2-5;
+  return mulResult;
+}
+ const result= mindGame(5);
+ console.log(result)
+
+//  Problem 2
+
+ 
