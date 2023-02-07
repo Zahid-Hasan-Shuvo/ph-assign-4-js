25665b68ff10d220175c211f88f64b0d2cfc1466
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-02-07 12:08:21 +0600

    all completed

--- index.js
+++ index.js
@@ -1,69 +1,98 @@
-//? Problem 1. Here gives an function that takes an positive number and doing some arithmetical operation. first mul with that num then add plus 10 ,then divided by 2. and at last sub by 5
+//? problem no:1. Here gives an function that takes an positive number and doing some arithmetical operation. Firstly multiply with that given number then add plus 10 ,then divided by 2. And at last Subtract 5 and return that result.
 
 
-// function mindGame(num){
-//   let mulResult=((num*3)+10)/2-5;
-//   return mulResult;
-// }
-//  const result= mindGame(5);
-// //  console.log(result)
+function mindGame(posNum){
+  let type= typeof posNum;
+  if(type!=="number"){
+    return "please give an valid number"
+  }
+  else if(posNum<0){
+    return "give an positive number"
+  }
 
-//!  Problem 2
-// function evenOdd(str){
- 
-//   for(let i=0; i<str.length; i++){
-   
-//     if(str.length%2==0){
-//       return 'even'
-//     }
-//     else{
-//       return 'odd';
-//     }
-//   }
+  let totalResult=((posNum*3)+10)/2-5;
+  return totalResult;
+}
 
  
-// }
-// const result=evenOdd('zahid hasan');
 
-//*  problem 3
 
-// function isLGSeven(num){
-//  const subNum= num-7;
-//  if(subNum<7){
-//   return subNum;
-//  }
-//  else if(subNum>=7){
-//    const mulNum=num*2;
-//    return mulNum;
-//  }
-// }
-//  const result=isLGSeven(15)
-//  console.log(result);
+//!  Problem No: 2. The output should be given based on the total number of characters in the String. If the character number is even, it will return 'even' and if it is odd, it will return 'odd'.
 
-//!  problem 4
-// function  findingBadData(numArray){
-//   let count=0;
-//   for(let i=0; i<numArray.length; i++){
-//    const element=numArray[i];
-//    if(element<0){
-//     count++
-//    }
-//   }
-//   return count;
+function evenOdd(str){
+  let type= typeof str;
+  if(type!=="string"){
+    return "please give an valid string"
+  }
+  for(let i=0; i<str.length; i++){
+    if(str.length%2==0){
+      return 'even'
+    }
+    else{
+      return 'odd';
+    }
+  }
+}
 
-// }
-// const numArray= [ -4, -9, -5, -33, -55 ]
-// const result=findingBadData(numArray);
-// console.log(result);
 
-//? problem 5
+//*  problem No:3. Take a number as the input value. Find the difference between that input value and 7. If this difference is less than 7, return the subtraction. Otherwise double the input must be returned.
 
-function gemsToDiamond(firstGem,secondGem,thirdGem){
- const first=firstGem*21;
- const second=secondGem*32;
- const third=thirdGem*43;
- const totalDiamond=first+second+third;
-const doubleTotal=(2*totalDiamond);
+function isLGSeven(num){
+
+  let type= typeof num;
+  if(type!=="number"){
+    return "please give an valid number"
+  }
+ const subNum= num-7;
+   if(subNum<7){
+  return subNum
+ }
+ else if (subNum>=7){
+   const doubleNum=num*2;
+   return doubleNum;
+ }
+}
+
+//!  problem No:4. Take an array as input. array element will be some number. Numbers can be negative numbers (less than zero) and positive numbers (greater than or equal to zero). If any number is negative it is “Bad Data”. If any number is positive it is “Good Data”. How many bad data are there in the array and must return that number.
+
+function  findingBadData(numArray){
+
+  let type= typeof numArray;
+  if(type!=="object"){
+    return "please give an valid input array"
+  }
+
+  let count=0;
+  for(let i=0; i<numArray.length; i++){
+   const element=numArray[i];
+   if(element<0){
+    count++
+   }
+  }
+  return count;
+}
+
+
+
+// ? problem No: 5. Firstly we have to declared a function named gemsToDiamond, which have 3 parameters. Thats parameter takes all num. 1st, 2nd and 3rd parameter multiply with 21,32,43 respectfully. Then all 3 multiply result will be added a total sum. If the total-sum<1000, then return that number. But if it cross double of the total-sum then subtraction 2000 from total-sum and return that.
+
+function gemsToDiamond(firstFriendGem,secondFriendGem,thirdFriendGem){
+
+  let type= typeof (firstFriendGem,secondFriendGem,thirdFriendGem);
+ 
+ 
+  if(type!=="number"){
+    return "please give all valid number"
+  }
+
+ const firstFriendDiamond=firstFriendGem*21;
+ const secondFriendDiamond=secondFriendGem*32;
+ const thirdFriendDiamond=thirdFriendGem*43;
+
+ const totalDiamond= firstFriendDiamond+secondFriendDiamond+thirdFriendDiamond;
+
+  const doubleTotal=(2*totalDiamond);
+
 
   if(totalDiamond<1000){
     return totalDiamond;
@@ -73,6 +102,3 @@ const doubleTotal=(2*totalDiamond);
    return newRe;
  
 }
-
-const result=gemsToDiamond(20, 200, 50)
-console.log(result);
